bfa3d7e27c9fda4582bc63458549257c594325e0
Leandro Bianchini <leandro.bianchini@sicpa.com>
2016-10-06 14:09:03 -0300

    Including Unit Test using Arquillian

--- src/main/java/com/example/javaee/rest/model/Person.java
+++ src/main/java/com/example/javaee/rest/model/Person.java
@@ -17,6 +17,8 @@ public class Person {
 	private Integer id;
 	
 	private String name;
+	private String lastName;
+	private Double salary;
 	
 	public Integer getId() {
 		return id;
@@ -30,15 +32,30 @@ public class Person {
 	public void setName(String name) {
 		this.name = name;
 	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public Double getSalary() {
+		return salary;
+	}
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
 	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
 		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((salary == null) ? 0 : salary.hashCode());
 		return result;
 	}
+	
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -53,17 +70,28 @@ public class Person {
 				return false;
 		} else if (!id.equals(other.id))
 			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
 		if (name == null) {
 			if (other.name != null)
 				return false;
 		} else if (!name.equals(other.name))
+			return false;
+		if (salary == null) {
+			if (other.salary != null)
+				return false;
+		} else if (!salary.equals(other.salary))
 			return false;
 		return true;
 	}
 	
 	@Override
 	public String toString() {
-		return "Person [id=" + id + ", name=" + name + "]";
+		return "Person [id=" + id + ", name=" + name + ", lastName=" + lastName + ", salary=" + salary + "]";
 	}
+	
 
 }
